d9d7e9de81b416029c4690bf24f2268048024a85
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-05-11 12:39:27 +0200

    Connection info til database

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -1,4 +1,28 @@
 package org.example.database;
 
+import java.sql.*;
+
 public class DatabaseConnection {
+
+private String database ="jdbc:mysql//https://itfag.usn.no/233574";
+
+private static final String brukernavn = "233574";
+private static final String pw = "JWeiMrF0";
+
+private Connection conn = null;
+private Statement stmt = null;
+private PreparedStatement pStmt = null;
+private ResultSet resSet = null;
+
+
+    public void open() throws SQLException {
+        try {
+            //Establish connection
+            conn = DriverManager.getConnection(database, brukernavn,pw);
+            //Create statement that will be used for executing SQL queries
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            ex.printStackTrace();// More elegant solutions for catching errors exist but they are out of the scope for this course
+        }
+    }
 }

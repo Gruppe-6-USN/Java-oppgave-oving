3ed5570b6f28ab6ebef817ff5b21c0153d590dd1
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-05-11 12:53:31 +0200

    Delete funksjon

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DatabaseConnection {
 
-private String database ="jdbc:mysql//https://itfag.usn.no/233574";
+private static final String database ="jdbc:mysql//https://itfag.usn.no/233574";
 
 private static final String brukernavn = "233574";
 private static final String pw = "JWeiMrF0";
@@ -23,6 +23,29 @@ private ResultSet resSet = null;
             stmt = conn.createStatement();
         } catch (SQLException ex) {
             ex.printStackTrace();// More elegant solutions for catching errors exist but they are out of the scope for this course
+        }
+    }
+
+    public void close() throws SQLException{
+        try {
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void deleteEmployee(int id) throws SQLException {
+        try {
+            open();
+            pStmt = conn.prepareStatement("delete from employees where id=?");
+            pStmt.setInt(1, id);
+
+            resSet = pStmt.executeQuery();
+
+            close();
+        } catch(SQLException err) {
+            err.printStackTrace();
         }
     }
 }

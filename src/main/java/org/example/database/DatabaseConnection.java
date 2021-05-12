e320b9cc7e60b90f6ca720ca9d79eea413be12bc
KristerLassen <70012970+KristerLassen@users.noreply.github.com>
2021-05-12 11:02:35 +0200

    properties

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -1,51 +1,133 @@
 package org.example.database;
 
-import java.sql.*;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
-private static final String database ="jdbc:mysql//https://itfag.usn.no/233574";
+	public static void main(String[] args) throws SQLException {
 
-private static final String brukernavn = "233574";
-private static final String pw = "JWeiMrF0";
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
 
-private Connection conn = null;
-private Statement stmt = null;
-private PreparedStatement pStmt = null;
-private ResultSet resSet = null;
+			// 1. Load the properties file
+			Properties props = new Properties();
+			props.load(new FileInputStream("demo.properties"));
+			// props.load(new FileInputStream("C:\Users\krist\Documents\GitHub\Java-oppgave-oving/demo.properties"));
 
+			// 2. Read the props
+			String theUser = props.getProperty("user");
+			String thePassword = props.getProperty("password");
+			String theDburl = props.getProperty("dburl");
+			
+			System.out.println("Connecting to database...");
+			System.out.println("Database URL: " + theDburl);
+			System.out.println("User: " + theUser);
+			
+			// 3. Get a connection to database
+			myConn = DriverManager.getConnection(theDburl, theUser, thePassword);
 
-    public void open() throws SQLException {
-        try {
-            //Establish connection
-            conn = DriverManager.getConnection(database, brukernavn,pw);
-            //Create statement that will be used for executing SQL queries
-            stmt = conn.createStatement();
-        } catch (SQLException ex) {
-            ex.printStackTrace();// More elegant solutions for catching errors exist but they are out of the scope for this course
-        }
-    }
+			System.out.println("\nConnection successful!\n");
+			
+			// 4. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 5. Execute SQL query
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			// 6. Process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
 
-    public void close() throws SQLException{
-        try {
-            stmt.close();
-            conn.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			close(myConn, myStmt, myRs);
+		}
+	}
 
-    public void deleteEmployee(int id) throws SQLException {
-        try {
-            open();
-            pStmt = conn.prepareStatement("delete from employees where id=?");
-            pStmt.setInt(1, id);
+	/**
+	 * Prompts the user. Return true if they enter "yes", false otherwise
+	 * 
+	 * @return
+	 */
+	private static boolean askUserIfOkToSave() {
+		Scanner scanner = new Scanner(System.in);
 
-            resSet = pStmt.executeQuery();
+		System.out.println("Is it okay to save?  yes/no: ");
+		String input = scanner.nextLine();
 
-            close();
-        } catch(SQLException deleteErr) {
-            deleteErr.printStackTrace();
-        }
-    }
+		scanner.close();
+
+		return input.equalsIgnoreCase("yes");
+	}
+
+	private static void showSalaries(Connection myConn, String theDepartment)
+			throws SQLException {
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+
+		System.out.println("Show Salaries for Department: " + theDepartment);
+
+		try {
+			// Prepare statement
+			myStmt = myConn
+					.prepareStatement("select * from employees where department=?");
+
+			myStmt.setString(1, theDepartment);
+
+			// Execute SQL query
+			myRs = myStmt.executeQuery();
+
+			// Process result set
+			while (myRs.next()) {
+				String lastName = myRs.getString("last_name");
+				String firstName = myRs.getString("first_name");
+				double salary = myRs.getDouble("salary");
+				String department = myRs.getString("department");
+
+				System.out.printf("%s, %s, %s, %.2f\n", lastName, firstName,
+						department, salary);
+			}
+
+			System.out.println();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			close(myStmt, myRs);
+		}
+
+	}
+
+	private static void close(Connection myConn, Statement myStmt,
+			ResultSet myRs) throws SQLException {
+		if (myRs != null) {
+			myRs.close();
+		}
+
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
+	}
+
+	private static void close(Statement myStmt, ResultSet myRs)
+			throws SQLException {
+
+		close(null, myStmt, myRs);
+	}
 }

96d4c99bf5030685d38e6e85ddae20f266aa586d
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-05-12 10:27:06 +0200

    Update MainInterface.java

--- src/main/java/org/example/gui/MainInterface.java
+++ src/main/java/org/example/gui/MainInterface.java
@@ -4,14 +4,5 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainInterface extends JFrame {
-    private org.example.gui.Update update= new org.example.gui.Update();
-    public MainInterface() {
-        setSize(400, 500);
-        setLocationRelativeTo(null);
-        setLayout(new GridLayout());
-
-        add(update);
-
-        setVisible(true);
-    }
+    
 }

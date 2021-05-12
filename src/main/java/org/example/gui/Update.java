72bd32cf6dfc1d2be6dea5f694829b823878a4e2
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-05-12 10:39:29 +0200

    Update Update.java

--- src/main/java/org/example/gui/Update.java
+++ src/main/java/org/example/gui/Update.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 
 public class Update extends JFrame {
 
+    //Labels
+    JLabel firstNameLabel = new JLabel("First name");
+    JLabel lastNameLabel = new JLabel("Last Name");
+    JLabel departmentLabel = new JLabel("Department");
+    JLabel emailLabel = new JLabel("Email");
+    JLabel salaryLabel = new JLabel("Salary");
+
+    //Fields
+
 
     public Update() {
 

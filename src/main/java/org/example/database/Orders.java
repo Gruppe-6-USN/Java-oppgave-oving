4ec5d19497a87ec0c85aaf77b16f5aed98d7c0f3
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-05-18 12:38:59 +0200

    Orders constructor

---
+++ src/main/java/org/example/database/Orders.java
@@ -0,0 +1,72 @@
+package org.example.database;
+
+public class Orders {
+    private int id, customerNumber;
+    private String orderDate, requiredDate, shippedDate, status, comments;
+
+    public Orders(int id, int customerNumber, String orderDate, String requiredDate, String shippedDate, String status, String comments) {
+        this.id = id;
+        this.customerNumber = customerNumber;
+        this.orderDate = orderDate;
+        this.requiredDate = requiredDate;
+        this.shippedDate = shippedDate;
+        this.status = status;
+        this.comments = comments;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(String requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public String getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(String shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
